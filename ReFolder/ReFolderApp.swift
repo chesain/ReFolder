91e86fef3d1adab4a2c26afd027db80c811cec20
chesain <cvanteddu1@gmail.com>
2025-07-16 14:45:22 -0500

    Initial Commit

---
+++ ReFolder/ReFolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReFolderApp.swift
+//  ReFolder
+//
+//  Created by Chetan Vanteddu on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ReFolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
